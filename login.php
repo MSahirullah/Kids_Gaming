afa99ad1cc971fdfef68d474591b9ba7f2f18a01
Shanuka <dilshansila@gmail.com>
2021-05-31 12:05:30 +0530

    Login page error message added.

--- login.php
+++ login.php
@@ -41,6 +41,17 @@ if (isset($_SESSION['login_user_email'])) {
                 <span class="login100-form-title">
                     <img src="../images/logo2.png" alt="logo" width="150" style="margin-left:-25px;padding-bottom: 30px;">
                 </span>
+                <?php
+                if ($_GET['error']) {
+                ?>
+                    <div class="text-center text-danger">
+                        <label class="label label-danger">
+                            Your Email or Password is invalid
+                        </label>
+                    </div>
+                <?php
+                }
+                ?>
 
                 <div class="wrap-input100 validate-input">
                     <input class="input100" type="text" name="email" required>
@@ -51,9 +62,9 @@ if (isset($_SESSION['login_user_email'])) {
                     <input class="input100" type="password" name="password">
                     <span class="focus-input100" data-placeholder="Password" required></span>
                 </div>
-
-                <a href="forgot-password.php" lass="link-light cp">Forgot Password? <i class="fas fa-arrow-right cp" style="margin-left:5px;margin-bottom:15px"></i></a>
-
+                <div class="text-center">
+                    <a href="forgot-password.php" lass="link-light cp">Forgot Password? <i class="fas fa-arrow-right cp" style="margin-left:5px;margin-bottom:15px"></i></a>
+                </div>
                 <div class="container-login100-form-btn">
                     <div class="wrap-login100-form-btn">
                         <div class="login100-form-bgbtn"></div>
